49059020f9f203ac7bea3e1f2ed1281c5b2ad5f7
Void.ID <romaissaeabde@gmail.com>
2024-10-27 11:52:15 +0100

    alry done, just some modif

--- Cpp Module 08/ex02/MutantStack.hpp
+++ Cpp Module 08/ex02/MutantStack.hpp
@@ -8,19 +8,18 @@
 # include <deque>
 # include <set>
 
-template <typename T>
-class MutantStack: public std::stack<T> {
+template <typename T, class Container = std::deque<T> >
+class MutantStack: public std::stack<T, Container> {
   public:
     MutantStack();
     MutantStack(const MutantStack& other);
     MutantStack& operator=(const MutantStack& other);
     ~MutantStack();
 
-    typedef typename std::stack<T>::container_type::iterator iterator;
-//     │             │               │              │        └─ New name
-//     │             │               │              └─ Iterator type from container
-//     │             │               └─ Type of container inside stack
-//     │             └─ Our template parameter
+    typedef typename Container::iterator iterator;
+//     │             │          │        └─ New type we're defining
+//     │             │          └─ Iterator type from container
+//     │             └─ Type of container inside stack
 //     └─ Tells compiler this is a type
 
     // -> inside the class, we can access 'c' using this->c
@@ -33,3 +32,22 @@ class MutantStack: public std::stack<T> {
       return this->c.end(); //
     };
 };
+
+template <typename T, class Container>
+MutantStack<T, Container>::MutantStack() {
+}
+
+template <typename T, class Container>
+MutantStack<T, Container>::MutantStack(const MutantStack& other): std::stack<T, Container>(other) {
+}
+
+template <typename T, class Container>
+MutantStack<T, Container>& MutantStack<T, Container>::operator=(const MutantStack& other) {
+  if (this != &other)
+    std::stack<T, Container>::operator=(other);
+  return *this;
+}
+
+template <typename T, class Container>
+MutantStack<T, Container>::~MutantStack() {
+}
